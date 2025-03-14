c39ac37aa4e8918fbf45f5381f5312d73b55e4f0
Rafhaela Marques <rafaelamarques_z@hotmail.com>
2025-03-14 18:06:17 -0300

    Add update endpoint for Kitchen with property copying and response handling

--- src/main/java/com/ionsistemas/foodapi/api/controller/KitchenController.java
+++ src/main/java/com/ionsistemas/foodapi/api/controller/KitchenController.java
@@ -2,7 +2,9 @@ package com.ionsistemas.foodapi.api.controller;
 
 import com.ionsistemas.foodapi.domain.model.Kitchen;
 import com.ionsistemas.foodapi.domain.service.KitchenService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,8 +32,23 @@ public class KitchenController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Kitchen> add(@RequestBody Kitchen kitchen) {
         return  ResponseEntity.ok(kitchenService.save(kitchen));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Kitchen> update(@PathVariable Long id, @RequestBody Kitchen kitchen){
+        Kitchen kitchenSaved = kitchenService.findById(id);
+
+        if (kitchenSaved != null) {
+            BeanUtils.copyProperties(kitchen, kitchenSaved, "id");
+            kitchenSaved = kitchenService.save(kitchenSaved);
+
+            return ResponseEntity.ok(kitchenSaved);
+        }
+
+        return ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/{id}")
